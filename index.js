c0c0920ba1aed6bd74d1be7f7631409e0a676ad1
Rapsutin <juho.lamminmaki@gmail.com>
2016-11-19 12:58:03 +0200

    Mock data

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Moi!');
+    res.send(JSON.stringify(
+        {
+            messages: [
+                'yy',
+                'kaaa'
+            ]
+        }
+    ));
 });
 
-app.listen(3000, '0.0.0.0', () => console.log('Up and running!'));
+app.listen(3000, () => console.log('Up and running!'));
