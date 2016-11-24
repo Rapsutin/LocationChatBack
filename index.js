c6ff4412734f550da7d7c2fa71eb690e36e4752c
Rapsutin <juho.lamminmaki@gmail.com>
2016-11-24 15:45:15 +0200

    Return json

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify(
+    res.send(
         {
             messages: [
                 'yy',
                 'kaaa'
             ]
         }
-    ));
+    );
 });
 
 app.listen(3000, () => console.log('Up and running!'));
